ef4791514584db390cabf82f9b825f10dc964c86
PyskoMaxym <pisko.maks@gmail.com>
2024-12-12 17:37:09 +0200

    fix profile card

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,32 +4,30 @@ import styles from "./Profile.module.css";
 const Profile = ({ name, tag, location, image, stats}) =>{
     return (
         <div className={styles.profile}>
-  <div className={styles.info}>
-    <img className={styles.imgprofile}
-      src={image}
-      alt="User avatar"
-    />
-    <p className={styles.name}>{name}</p>
-    <p className={styles.tag}>@{tag}</p>
-    <p className={styles.location}>{location}</p>
-  </div>
-
-  <ul className={styles.list}>
-    <li className={styles.label}>
-      <span>Followers</span>
-      <span className={styles.numbers}>{stats.followers}</span>
-    </li>
-    <li className={styles.label}>
-      <span>Views</span>
-      <span className={styles.numbers}>{stats.views}</span>
-    </li>
-    <li className={styles.label}>
-      <span>Likes</span>
-      <span className={styles.numbers}>{stats.likes}</span>
-    </li>
-  </ul>
+ <div className={styles.info}>
+  <img className={styles.imgprofile}
+   src={image}
+   alt="User avatar"
+  />
+  <p className={styles.name}>{name}</p>
+ <p className={styles.tag}>@{tag}</p>
+  <p className={styles.location}>{location}</p>
+ </div>
+ <ul className={styles.list}>
+  <li className={styles.label}>
+   <span>Followers</span>
+   <span className={styles.numbers}>{stats.followers}</span>
+  </li>
+  <li className={styles.label}>
+   <span>Views</span>
+   <span className={styles.numbers}>{stats.views}</span>
+  </li>
+  <li className={styles.label}>
+   <span>Likes</span>
+   <span className={styles.numbers}>{stats.likes}</span>
+  </li>
+ </ul>
 </div>
-
     )
 }
 
